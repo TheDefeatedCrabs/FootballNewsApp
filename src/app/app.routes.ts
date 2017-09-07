5b9a0ec7f3fc3550f8a4cc6e1b0034d16dc7f13b
Georgi <g.borisov123@gmail.com>
2017-09-07 13:54:23 +0300

    User Profile template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,14 +16,14 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'news/all', component: NewsComponent },
-    { path: 'news/add', component: AddNewsComponent },
+    { path: 'news/add', component: AddNewsComponent, canActivate: [AuthGuard] },
     { path: 'news/:id', component: NewsDetailsComponent },
     { path: 'news/edit/:id', component: EditNewsComponent },
     { path: 'tips/all', component: TipsComponent},
     { path: 'tips/add', component: AddTipComponent, canActivate: [AuthGuard] },
     { path: 'register', component: RegisterComponent},
     { path: 'login', component: LoginComponent },
-    { path: 'profile', component: ProfileComponent }
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
