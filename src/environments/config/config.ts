66cd1f13e5894f163e20ccf1fd4b74d21205f4ba
Gancho Chankov <gchankov@gmail.com>
2017-09-08 06:40:53 +0300

    Adding configs back to branch.

---
+++ src/environments/config/config.ts
@@ -0,0 +1,12 @@
+export const Config = {
+    FirebaseConfig: {
+        apiKey: "AIzaSyAHvI_4TeAPLEVp1VEme9feDKgh6U1kR80",
+        authDomain: "footballapp-f9fad.firebaseapp.com",
+        databaseURL: "https://footballapp-f9fad.firebaseio.com",
+        storageBucket: "",
+        messagingSenderId: "25155396791"
+    },
+    FootballApiConfig: {
+        API_KEY: "373f6fcc7c614ae7a269bf364d51618f"
+    }
+}
